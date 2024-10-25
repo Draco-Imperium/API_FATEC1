606efea331fa866458777c6cfc826cb11d7d6c9f
Lucas Marins <lucasmarins2024@gmail.com>
2024-10-25 12:14:32 -0300

    UPDATE

--- app/app.py
+++ app/app.py
@@ -3,16 +3,18 @@ from flask_caching import Cache
 import json
 import os
 import sys
-import unidecode
 from getFunctions import *
+import unidecode
+import logging
 
 sys.setrecursionlimit(1500)
+
+logging.basicConfig(filename='app.log',level=logging.INFO,format='%(asctime)s-%(levelname)s-%(message)s')
 
 app = Flask(__name__)
 cache = Cache(app, config={'CACHE_TYPE': 'simple'})
 
 def format_name(name):
-    """Remove acentos, converte para minúsculas e substitui espaços por underscores."""
     return unidecode.unidecode(name).lower().replace(" ", "_")
 
 app.jinja_env.filters['format_name'] = format_name
@@ -24,6 +26,7 @@ def index():
         leis = json.load(file)      
     leis = leis['leis']
     ultimas = prepos() 
+<<<<<<< Updated upstream
     return render_template('index.html', ultimas=ultimas, leis=leis )
 
     
@@ -40,6 +43,9 @@ def get_dados2():
     except json.JSONDecodeError:
         return jsonify({"mensagem": "Erro ao decodificar JSON"}), 500
 
+=======
+    return render_template('index.html', ultimas=ultimas)
+>>>>>>> Stashed changes
 
 @app.route('/proposicoes')
 def proposicoes():
@@ -71,8 +77,10 @@ def get_dados():
             data = json.load(file)
         return jsonify(data)
     except FileNotFoundError:
+        logging.error("Arquivo não encontrado")
         return jsonify({"mensagem": "Arquivo não encontrado"}), 404
-    except json.JSONDecodeError:
+    except json.JSONDecodeError as e:
+        logging.error("Erro ao decodificar JSON: %s", e)
         return jsonify({"mensagem": "Erro ao decodificar JSON"}), 500
 
 @app.route('/usuarios/<int:id>', methods=['GET'])
@@ -89,8 +97,10 @@ def get_usuario(id):
         else:
             return jsonify({"mensagem": "Usuário não encontrado"}), 404
     except FileNotFoundError:
+        logging.error("Arquivo não encontrado")
         return jsonify({"mensagem": "Arquivo não encontrado"}), 404
     except json.JSONDecodeError:
+        logging.error("Erro ao decodificar JSON")
         return jsonify({"mensagem": "Erro ao decodificar JSON"}), 500
 
 @app.route('/vereadores_geral')
@@ -100,16 +110,21 @@ def vereadores_geral():
         file_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'database', 'data.json')
         with open(file_path, 'r', encoding='utf-8', errors='replace') as file:
             data = json.load(file)
-        parlamentares = data['parlamentares']
-        return render_template('vereadores_geral.html', parlamentares=parlamentares)
+
+        for parlamentar in data['parlamentares']:
+            parlamentar['DS_SIT_TOT_TURNO'] = parlamentar['DS_SIT_TOT_TURNO'].replace('\r\n\r\n', '<br>').replace('\r\n', '<br>')
+
+
+
+        return render_template('vereadores_geral.html', parlamentares=data['parlamentares'])
     except FileNotFoundError:
         print("Arquivo não encontrado:", file_path)
         return render_template('404.html'), 404
     except json.JSONDecodeError:
         return render_template('500.html'), 500
 
+
 @app.route('/perfil/<string:NM_URNA_CANDIDATO>')
-# @cache.cached(timeout=300)
 def perfil(NM_URNA_CANDIDATO):
     try:
         file_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'database', 'data.json')
@@ -119,14 +134,30 @@ def perfil(NM_URNA_CANDIDATO):
         parlamentar = next((p for p in data['parlamentares'] if format_name(p['NM_URNA_CANDIDATO']) == NM_URNA_CANDIDATO), None)
         
         if parlamentar is None:
-            print(f"Parlamentar com NM_URNA_CANDIDATO '{NM_URNA_CANDIDATO}' não encontrado.")
+            logging.warning(f"Parlamentar com NM_URNA_CANDIDATO '{NM_URNA_CANDIDATO}' não encontrado.")
             return render_template('404.html'), 404
         
         return render_template('perfil.html', parlamentar=parlamentar)
     except FileNotFoundError:
+        logging.error("Arquivo não encontrado")
         return render_template('404.html'), 404
     except json.JSONDecodeError:
+        logging.error("Erro ao decodificar JSON")
         return render_template('500.html'), 500
+    
+@app.errorhandler(404)
+def not_found(error):
+    logging.error(f"Página não encontrada: {error}")
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def internal_error(error):
+    logging.error(f"Erro interno: {error}")
+    return render_template('500.html'), 500
+
+@app.route('/simular_erro')
+def simular_erro():
+    raise Exception("Este é um erro simulado para testar o tratamento de erros 500.")
 
 if __name__ == '__main__':
     app.run(debug=True)
